e37ab8193b2d0be81c6153a186bd28f23fa797f3
CondeArmand <104652384+CondeArmand@users.noreply.github.com>
2022-07-01 16:36:33 -0300

    It's ok

--- Curso de ADS/Projetos do JP/Projeto001.py
+++ Curso de ADS/Projetos do JP/Projeto001.py
@@ -1,15 +1,17 @@
 from Functions import crie_matriz, linha
 import numpy as np
+# Entrada de dados
 n = int(input('Informe uma matriz quadrada válida: '))
 linha()
-matrizA = crie_matriz(n, n)
+matrizA = crie_matriz(n, n) # Cria uma matriz quadrada, com os valores indicados pelo usuário
 linha()
-matrizB = crie_matriz(n, n)
+matrizB = crie_matriz(n, n) # Cria uma matriz quadrada, com os valores indicados pelo usuário
 linha()
-matrizBT = np.transpose(matrizB)
-x = np.trace(matrizA)
+# calculando...
+matrizBT = np.transpose(matrizB) # gera a transposta da matriz B
+x = np.trace(matrizA) # soma a diagonal principal da matriz A
 matrizA = np.array(matrizA)
-matrizXA = np.multiply(matrizA, x)
-matrizR = np.matmul(matrizXA, matrizBT)
+matrizXA = np.multiply(matrizA, x) # Multiplica a Matriz A com X
+matrizR = np.matmul(matrizXA, matrizBT) # Calcula a resultante entre a operação anterior com a transposta da matriz B
 print(matrizR)
 linha()
